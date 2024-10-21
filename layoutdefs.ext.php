350071dfae159090c69a32dd0df0a8070b9783e0
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2024-10-21 20:00:23 +0530

    adsense

---
+++ layoutdefs.ext.php
@@ -0,0 +1,32 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+ 
+$layout_defs['Accounts']['subpanel_setup']['opp_closed_lost'] =
+        array('order' => 49,
+            'module' => 'Opportunities',
+            'subpanel_name' => 'ForAccounts',
+            'get_subpanel_data' => 'function:get_closed_lost_closed_won_opportunities',
+            'generate_select' => true,
+            'title_key' => 'Account Feed',
+            'top_buttons' => array(),
+            'function_parameters' => array(
+                'import_function_file' => 'custom/modules/Accounts/customOpportunitiesSubpanel.php',
+                'sales_stage' => 'Closed Lost',
+                'account_id' => $this->_focus->id,
+                'return_as_array' => 'true'
+            ),
+);
+
+//auto-generated file DO NOT EDIT
+$layout_defs['Accounts']['subpanel_setup']['documents']['override_subpanel_name'] = 'Account_subpanel_documents';
+
+
+//auto-generated file DO NOT EDIT
+$layout_defs['Accounts']['subpanel_setup']['account_aos_contracts']['override_subpanel_name'] = 'Account_subpanel_account_aos_contracts';
+
+
+//auto-generated file DO NOT EDIT
+$layout_defs['Accounts']['subpanel_setup']['contacts']['override_subpanel_name'] = 'Account_subpanel_contacts';
+
+?>
--- resources/views/adsense.blade.php
+++ resources/views/adsense.blade.php
@@ -1,2 +1,2 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3325590995381694"
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9094681624597236"
      crossorigin="anonymous"></script>
